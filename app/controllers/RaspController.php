9e9053430b3ee763c16d96da07e1c7520bb45fdc
Adam Gieron <adam.gieron@magently.com>
2017-06-10 13:19:19 +0200

    Add audio controller actions

--- app/controllers/RaspController.php
+++ app/controllers/RaspController.php
@@ -9,9 +9,24 @@ use Illuminate\Http\Response as Response;
 class RaspController extends BaseController 
 {
 	private $process;
+	private $audioprocess;
 	private $output;
+	private $playerOutput;
 	private $command;
+	
 //sudo chmod 4755 /usr/sbin/i2cdetect /usr/sbin/i2cset /usr/sbin/i2cget /usr/sbin/i2cdump
+	public function playAudio($command) 
+	{	
+		$this->audioprocess = new Process('mpg123 '.$command);
+
+		try {
+			$this->audioprocess->mustRun();
+			return $this->playerOutput = $this->audioprocess->getOutput();
+		} catch(ProcessFailedException $e) {
+			return $this->output = $e->getMessage();
+		}
+	}
+
 	public function getAjaxRequest()
 	{
 		$req = new Request();
@@ -30,13 +45,13 @@ class RaspController extends BaseController
 		$comProcess = substr($comProcess,0,-10);
 		$hexaddress = '0x'.(dechex(bindec($toHex)));
 		$this->command = '/usr/sbin/i2cset -y 1 '.$comProcess.' '.$hexaddress;
-		return $this->executeProcess();
+		$this->executeProcess();
+		return $this->playAudio($comProcess.$hexaddress);
 	}
 
 	public function executeProcess()
 	{
 		$this->process = new Process($this->command);
-		var_dump($this->command);
 		try {
     		$this->process->mustRun();
 			return $this->output = $this->process->getOutput();
