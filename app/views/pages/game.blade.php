f16307de1275eb2cb100fe10e44502a93e9c60ec
Adam Gieron <adam.gieron@fastwhitecat.com>
2018-03-07 19:15:06 +0100

    answers

--- app/views/pages/game.blade.php
+++ app/views/pages/game.blade.php
@@ -92,9 +92,6 @@
                 <a href="#" class="expanders__relay relayexp1"
                    audio="pliszkasiwa.mp3"
                    answer="pliszkasiwa.mp3"
-                   relayId="relay1"
-                   expander="0x24 0x01 "
-                   external="false"
                    pin="1">pliszka siwa </a>
                 <input type="hidden" value="Pliszka siwa to najpospolitszy ptak Europy. Zamieszkuje tereny nadwodne, gdzie żeruje, łowiąc owady; może się jednak znacznie oddalać od zbiorników - spotykana jest żerująca przy drogach, na rozległych łąkach, czy też polach uprawnych, gdzie biega za pługiem i zbiera wykopane larwy i pędraki. Chodząc po ziemi, pliszki charakterystycznie kiwają ogonem w dół i w górę. Ich gniazda ukryte są na ziemi lub w różnych szczelinach i otworach, także budynków; pliszki są mało płochliwe i chętnie trzymają się w pobliżu człowieka. Tworzą przeważnie niewielkie stadka, którymi lecą na zimowiska na południe Europy lub do Afryki Północnej.
 " class="text-to-append dispNone"/>
@@ -102,10 +99,7 @@
             <li>
                 <a href="#" class="expanders__relay relayexp1"
                    audio="pleszka.mp3"
-                   answer=""
-
-                   expander="0x24 0x01 "
-                   external="false"
+                   answer="pleszka.mp3"
                    pin="1">pleszka </a>
                 <input type="hidden" value="Pleszka, z rodziny muchołówkowatych, występuje na niżu kraju. Nie jest ona ptakiem licznym i pomimo jej barwnego upierzenia w terenie nie rzuca się w oczy. Zamieszkuje przede wszystkim obrzeża lasów liściastych i mieszanych, ale chętnie przebywa też w pobliżu osad ludzkich w zieleni miejskiej i wiejskiej. Gniazduje w obszernej dziupli lub w skrzynce lęgowej. Pleszki żywią się głównie owadami, które chwytają w locie lub zbierają z liści i ziemi, a jesienią zjadają też owoce. Migrują na duże odległości - zimują w Afryce Północnej.
 			" class="text-to-append dispNone"/>
@@ -114,9 +108,6 @@
                 <a href="#" class="expanders__relay relayexp1"
                    audio="kopciuszek.mp3"
                    answer="kopciuszek.mp3"
-
-                   expander="0x24 0x01 "
-                   external="false"
                    pin="1"> kopciuszek</a>
                 <input type="hidden" value="Kopciuszek z rodziny drozdowatych to wędrowny ptak, występujący średnio licznie na całym obszarze kraju. Nazwa gatunku odnosi się do szarego ubarwienia u samic i ciemnoszarego - u samców; przez tę kolorystykę kopciuszek nazywany jest też kominiarczykiem.Przystosował się on świetnie się do życia mieście - zabudowa miejska stanowi dla niego namiastkę górskich terenów, na których pierwotnie gniazdował - występuje nawet w centrach wielkich miast, gdzie przy sztucznym oświetleniu jego głos rozbrzmiewa nawet w nocy. Dość rzadko jest za to spotykany na terenach wiejskich. Gniazda buduje z traw i korzonków w szczelinach skał lub budynków, pokarmu za to szuka na ziemi - zjada owady i inne bezkręgowce, jesienią również jagody.
 			" class="text-to-append dispNone"/>
@@ -125,9 +116,6 @@
                 <a href="#" class="expanders__relay relayexp1"
                    audio="czyzyk.mp3"
                    answer="czyzyk.mp3"
-
-                   expander="0x24 0x01 "
-                   external="false"
                    pin="1">Czyżyk</a>
                 <input type="hidden" value="Czyżyk, ptak z rodziny łuszczykowatych, dość licznie występuje w całym paśmie gór, a nielicznie w pozostałej części Polski. Zimuje w kraju. W okresie lęgowym przebywa w borach iglastych, swoje gniazdo buduje na wysokości do kilkunastu metrów, tak dobrze ukrywając je w gęstych gałązkach świerka lub jodły, że niezbyt wielu ornitologów miało okazję je zobaczyć. Nie zapuszcza się też na bezdrzewne tereny. Latem odżywia się zarówno nasionami, jak i owadami, a jesienią i zimą trzyma się w olszynach, gdyż jego pokarmem w tym okresie są głównie nasiona olchy. Czyżyki żerują zazwyczaj w stadkach, często się do siebie odzywając.
 					" class="text-to-append dispNone"/>
@@ -136,9 +124,6 @@
                 <a href="#" class="expanders__relay relayexp1"
                    audio="mysikrolik.mp3"
                    answer="mysikrolik.mp3"
-
-                   expander="0x24 0x01 "
-                   external="false"
                    pin="1">mysikrólik </a>
                 <input type="hidden" value="Mysikrólik to najmniejszy ptak Europy, zamieszkujący bory świerkowe, jodłowe lub mieszane, rzadziej sosnowe. Jak na tak maleńkiego ptaka, buduje dość masywne, zamknięte z góry gniazda, które podwiesza pod gałęzią w koronie drzewa. Zimuje na południu Europy, północny Afryki, a nawet w Ameryce Północnej. Podczas toków samiec unosi swoje barwne pióra na wierzchu głowy, strosząc je w czubek. Polując na gałęziach na drobne bezkręgowce, głównie owady, często zawisa głową w dół. Na ziemię schodzi tylko wyjątkowo, w sytuacji znacznego głodu; ciężko jest więc go zauważyć, ale za to usłyszeć można jego wysoki, falujący śpiew.
 					" class="text-to-append dispNone"/>
@@ -147,9 +132,6 @@
                 <a href="#" class="expanders__relay relayexp1"
                    audio="strzyzyk.mp3"
                    answer="strzyzyk.mp3"
-
-                   expander="0x24 0x01 "
-                   external="true"
                    pin="1"> strzyżyk</a>
                 <input type="hidden" value="Strzyżyk zwyczajny, o charakterystycznym zadartym ogonku, występuje na niżu kraju i jest jednym z naszych najmniejszych ptaków. Żyje w lasach liściastych i mieszanych z bujnym podszytem; choć nie jest ptakiem rzadkim, ciężko go zobaczyć - trzyma się gęstych zarośli, jest skryty i rzadko pokazuje się na otwartych miejscach. Mimo swych niewielkich rozmiarów śpiewa wyjątkowo donośnym głosem i buduje stosunkowo duże gniazdo o kulistym kształcie, ukryte nisko wśród gęstych gałęzi. Gdy szuka pokarmu - owadów i innych bezkręgowców - prześlizguje się pomiędzy krzewami jak mysz.
 					" class="text-to-append dispNone"/>
@@ -158,9 +140,6 @@
                 <a href="#" class="expanders__relay relayexp1"
                    audio="pierwiosnek.mp3"
                    answer="pierwiosnek.mp3"
-
-                   expander="0x24 0x01 "
-                   external="false"
                    pin="1"> pierwiosnek</a>
                 <input type="hidden" value="Pierwiosnek z rodziny świstunek występuje licznie w całym kraju, zamieszkuąc różnego typu prześwietlone lasy i zadrzewienia. Ptak ten buduje kuliste gniazdo z otworem z boku, które ukrywa w gęstej roślinności nisko przy ziemi. Rozbrzmiewający wczesną wiosną śpiew pierwiosnka jest nie do pomylenia z żadnym innym ptakiem. Żeruje on, przemieszczając się pomiędzy krzewami i koronami drzew, choć nierzadko łapie owady również w locie; jesienią zajada też jagody. Krajowe pierwiosnki migrują na zimę w rejon śródziemnomorski i do Afryki.
 					" class="text-to-append dispNone"/>
@@ -169,8 +148,6 @@
                 <a href="#" class="expanders__relay relayexp1"
                    audio="mucholowkaszara.mp3"
                    answer="mucholowkaszara.mp3"
-                   expander="0x24 0x01 "
-                   external="false"
                    pin="1">muchołówka szara</a>
                 <input type="hidden" value="Muchołówka szara w Polsce jest średnio liczna i wędrowna - zimuje w Afryce. W prześwietlonych lasach różnego typu, starych parkach i ogrodach znajduje sobie dziuple lub skrzynki lęgowe, w których gniazduje. Siedzącą spokojnie muchołówkę trudno zauważyć w lesie - rzuca się w oczy dopiero, gdy zwinnym lotem chwyta w powietrzu owada. Również jej śpiew jest skromny i cichy, dlatego mimo iż pospolita, pozostaje ptakiem słabo znanym.
 			" class="text-to-append dispNone"/>
