356a5fddb9693d623f79c784d194f70aa7db877c
Adam Gieron <adam.gieron@fastwhitecat.com>
2018-04-26 16:44:15 +0200

    test

--- html/js/custom.js
+++ html/js/custom.js
@@ -1,116 +1,116 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function ($) {
 
-    var pinLogic = function() {
+    var pinLogic = function () {
         console.log('pin logic');
         var button = $('.expanders__relay');
-        button.each(function(){
-            if ($(this).attr('audio') == '.mp3'){
-                $(this).css('display','none');
+        button.each(function () {
+            if ($(this).attr('audio') == '.mp3') {
+                $(this).css('display', 'none');
             } else {
-                $(this).css('display','block');
+                $(this).css('display', 'block');
             }
         });
 
-        function setButtons(){
-            button.each(function(){
-                $(this).on('click',function(){
-                    if($(this).attr('pin') == '1'){
+        function setButtons() {
+            button.each(function () {
+                $(this).on('click', function () {
+                    if ($(this).attr('pin') == '1') {
                         $(this).removeClass('on');
                     } else {
                         $(this).addClass('on');
                     }
                 });
             });
-        } 
+        }
 
-        function getObject()
-        {
+        function getObject() {
 
             return $('.expanders > li > a');
         }
 
-        function pins(obj)
-        {
-            getObject().each(function(index){
-                $(this).on('click',function(){
+        function pins(obj) {
+            getObject().each(function (index) {
+                $(this).on('click', function () {
                     var id = $(this).attr('relayId')
-                        text = $(this).next().val()
-                        content = $('#custom-text');
-                        content.html('');
-                        content.html(text);
-                    $(this).attr('pin', function(index, attr){
+                    text = $(this).next().val()
+                    content = $('#custom-text');
+                    content.html('');
+                    content.html(text);
+                    $(this).attr('pin', function (index, attr) {
                         return attr == 1 ? 0 : 1;
                     });
                     var expander = [],
                         pointData = [],
                         command = $(this).attr('expander');
-                        command2 = getObject().each(function(){
-                                        if(command == $(this).attr('expander')) {
-                                            expander.push($(this).attr('pin'));
-                                        }
-                                    });
-                    var stringPin = expander.join('');
-                    
-                        stringProcessPin = stringPin.replace(',','');
-                        pointData[0] = command + stringProcessPin;
-                        pointData[1] = $(this).attr('audio');
-                        pointData[2] = 'status' + $(this).attr('pin');
-
-                        if ($(this).attr('external') == 'true'){
-                            console.log('external ajax');
-                            $.ajax({
-                                type: "GET",
-                                url : "send",
-                                data : { data: pointData },
-                                success : function(response){
-                                    console.log(response);
-                                    $.ajax({
-                                        type: "POST",
-                                        url : "setpoint",
-                                        data : { data: pointData },
-                                        success : function(response){
-                                            console.log(response)
-                                            console.log(pointData)
-                                        },error:function(resp) {
-                                            console.log(resp)
-                                        }
-                                    },"json");
-                                },error:function(resp){
-                                    console.log(resp)
-					console.log('error but im run mp3 lalala:)');
-				     $.ajax({
-                                        type: "POST",
-                                        url : "setpoint",
-                                        data : { data: pointData },
-                                        success : function(response){
-                                            console.log(response)
-                                            console.log(pointData)
-                                        },error:function(resp) {
-                                            console.log(resp)
-                                        }
-                                    },"json");
-                                }
-                            },"json");
-
-                        } else {
-                            console.log('internal ajax');
-                            $.ajax({
-                                type: "POST",
-                                url : "setpoint",
-                                data : { data: pointData },
-                                success : function(response){
-                                    console.log(pointData)
-                                },error:function(resp) {
-                                    console.log(resp)
-                                }
-                            },"json");
+                    command2 = getObject().each(function () {
+                        if (command == $(this).attr('expander')) {
+                            expander.push($(this).attr('pin'));
                         }
+                    });
+                    var stringPin = expander.join('');
+
+                    stringProcessPin = stringPin.replace(',', '');
+                    pointData[0] = command + stringProcessPin;
+                    pointData[1] = $(this).attr('audio');
+                    pointData[2] = 'status' + $(this).attr('pin');
+
+                    if ($(this).attr('external') == 'true') {
+                        console.log('external ajax');
+                        $.ajax({
+                            type: "GET",
+                            url: "send",
+                            data: {data: pointData},
+                            success: function (response) {
+                                console.log(response);
+                                $.ajax({
+                                    type: "POST",
+                                    url: "setpoint",
+                                    data: {data: pointData},
+                                    success: function (response) {
+                                        console.log(response)
+                                        console.log(pointData)
+                                    }, error: function (resp) {
+                                        console.log(resp)
+                                    }
+                                }, "json");
+                            }, error: function (resp) {
+                                console.log(resp)
+                                console.log('error but im run mp3 lalala:)');
+                                $.ajax({
+                                    type: "POST",
+                                    url: "setpoint",
+                                    data: {data: pointData},
+                                    success: function (response) {
+                                        console.log(response)
+                                        console.log(pointData)
+                                    }, error: function (resp) {
+                                        console.log(resp)
+                                    }
+                                }, "json");
+                            }
+                        }, "json");
+
+                    } else {
+                        console.log('internal ajax');
+                        $.ajax({
+                            type: "POST",
+                            url: "setpoint",
+                            data: {data: pointData},
+                            success: function (response) {
+                                console.log(pointData)
+                            }, error: function (resp) {
+                                console.log(resp)
+                            }
+                        }, "json");
+                    }
 
 
                 });
             });
             setButtons();
-        } pins();
+        }
+
+        pins();
         //returns
         return {
             setButtons: setButtons,
@@ -118,81 +118,79 @@ jQuery(document).ready(function($){
         }
     }();
 
-    var pullLogic = function(){
+    var pullLogic = function () {
 
-        function pull(){
+        function pull() {
             var pull = $('.branch')
-                output = $('#output');
-            pull.on('click',function(){
+            output = $('#output');
+            pull.on('click', function () {
                 var br = $(this).attr('branch');
                 pull.css("display", "none");
                 $.ajax({
                     type: "POST",
-                    url : "fetchData",
-                    data : { data: br },
-                    success : function(response){
+                    url: "fetchData",
+                    data: {data: br},
+                    success: function (response) {
                         pull.css("display", "block");
                         console.log(response);
                         output.html();
                         output.html(response);
-                    }, error: function(response)
-                    {
+                    }, error: function (response) {
                         pull.css("display", "block");
                         console.log(response);
                         output.html();
                         output.html(response.responseText);
                     }
-                },"json");
+                }, "json");
             });
         }
+
         pull();
         return {
             pull: pull
         }
     }();
 
-    var switchall = function(){
+    var switchall = function () {
         var switchAllOn = $('#switchAllOn'),
             switchAllOff = $('#switchAllOff');
-            console.log(switchAllOff);
-        switchAllOn.on('click',function(){
+        console.log(switchAllOff);
+        switchAllOn.on('click', function () {
             switchAllOff.removeClass('on');
 
             $.ajax({
                 type: "POST",
-                url : "switchAllOn",
-                data : { data: 'on' },
-                success : function(response){
+                url: "switchAllOn",
+                data: {data: 'on'},
+                success: function (response) {
                     console.log(response);
-                }, error: function(response)
-                {
+                }, error: function (response) {
                     console.log(response);
                 }
-            },"json");
+            }, "json");
 
         });
-        switchAllOff.on('click',function(){
+        switchAllOff.on('click', function () {
             switchAllOn.removeClass('on');
             console.log(switchAllOn);
             $.ajax({
                 type: "POST",
-                url : "switchAllOff",
-                data : { data: 'off' },
-                success : function(response){
+                url: "switchAllOff",
+                data: {data: 'off'},
+                success: function (response) {
                     console.log(response);
-                }, error: function(response)
-                {
+                }, error: function (response) {
                     console.log(response);
                 }
-            },"json");
+            }, "json");
         });
 
     }();
 
-    var setText = function() {
+    var setText = function () {
         var text = $(this).next().val(),
             content = $('#custom-text');
-            button = $();
+        button = $();
 
     }();
 });
