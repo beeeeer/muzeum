27188f283e9547bfd073902ef15e500ef84c4f67
Adam Gieron <adam.gieron@fastwhitecat.com>
2018-04-27 00:28:40 +0200

    test

--- app/commands/forestFire.php
+++ app/commands/forestFire.php
@@ -9,99 +9,93 @@ use Illuminate\Http\Request as Request;
 use Illuminate\Http\Response as Response;
 use GuzzleHttp\Client as Client;
 
-class forestFire extends Command {
+class forestFire extends Command
+{
 
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'forest:fire';
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Starts forest fire';
     private $fire_flag;
+    private $running = false;
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'forest:fire';
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Starts forest fire';
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
+        if($this->running == true) return;
 
-	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		parent::__construct();
-	}
-
-	/**
-	 * Execute the console command.
-	 *
-	 * @return mixed
-	 */
-	public function fire()
-	{
-	    if($this->fire_flag == 0) {
-	        return;
-        }
-
-	    $process = new Process('gpio -g read 16');
-	    $process->mustRun();
+        $process = new Process('gpio -g read 16');
+        $process->mustRun();
         $this->fire_flag = $process->getOutput();
-	    if (($this->fire_flag == 0))
-        {
+        if($this->fire_flag ==1) $this->running =false;
+        if ($this->fire_flag == 0) {
+            $this->running == true;
             $process = new Process('curl http://192.168.0.76/index.php/fireprocess');
             $process->mustRun();
-            if ($process->getOutput() == 'done'){
-                return;
+            if($process->getOutput()){
+
             }
         }
-	}
+
+    }
 
     public function airPlane()
     {
         $process = new Process('gpio -g read 21');
         $process->mustRun();
-
-	}
+        return $process->getOutput();
+    }
 
     public function water()
     {
         $process = new Process('gpio -g read 20');
         $process->mustRun();
-	}
+    }
 
 
-	/**
-	 * Get the console command arguments.
-	 *
-	 * @return array
-	 */
-	protected function getArguments()
-	{
-		return array();
-	}
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return array();
+    }
 
-	/**
-	 * Get the console command options.
-	 *
-	 * @return array
-	 */
-	protected function getOptions()
-	{
-		return array(
-			array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
-		);
-	}
-
-
-
-
-
-
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return array(
+            array('example', null, InputOption::VALUE_OPTIONAL, 'An example option.', null),
+        );
+    }
 
 
 }
