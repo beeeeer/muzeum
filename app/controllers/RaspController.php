f0634c500d1887b09f82ea28dc46f691e36fce2a
root <you@example.com>
2018-04-27 17:20:34 +0000

    Last

--- app/controllers/RaspController.php
+++ app/controllers/RaspController.php
@@ -118,6 +118,7 @@ class RaspController extends BaseController
 
     public function switchallOff()
     {
+	$this->allExp('pkill mpg123')->run();
         $this->allExp('/usr/sbin/i2cset -y 1 0x22 0x00 0xff')->run();
         $this->allExp('/usr/sbin/i2cset -y 1 0x22 0x01 0xff')->run();
         $this->allExp('curl 192.168.0.79/index.php/swoff')->run();
